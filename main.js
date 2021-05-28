a4f86888774bcaa83ac6a4c9682bf77dfdebaf8b
kento-777 <kentuuuwork@gmail.com>
2021-05-28 23:47:03 +0900

    完成版 ボーダーの修正

--- main.js
+++ main.js
@@ -4,4 +4,13 @@ $(function(){
     $(".border1",this).toggleClass('open1');
     $(".border2",this).toggleClass('open2').toggleClass('close');
   });
+
+  $('#programmer').on("click", function() {
+    $(this).children("p").toggleClass('active').toggleClass('non-active');
+  });
+  
+  
+  
+
+
 });
