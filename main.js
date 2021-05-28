2eec314c1c8141abb87caebe099fac35d627a177
kento-777 <kentuuuwork@gmail.com>
2021-05-28 15:58:08 +0900

    背景色の調整

--- main.js
+++ main.js
@@ -1,9 +1,7 @@
 $(function(){
   $('.description__logo').on("click", function() {
-    $(this).next().slideToggle().toggleClass('isActive');
-    $(".border1",this).toggleClass('open1')
+    $(this).toggleClass('isActive').next().slideToggle();
+    $(".border1",this).toggleClass('open1');
     $(".border2",this).toggleClass('open2').toggleClass('close');
   });
-
-  
 });
